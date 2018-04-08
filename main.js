dc2e7583bb032feb32af2cacc5d0e6cc020808dd
Terrance Corley <terrancecorley@outlook.com>
2018-04-08 12:21:31 -0700

    added boilerplate for editing functionality of list items

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 'use strict';
 
+/* global $ */
+
 const STORE = {
   items: [
-		{name: "oranges", checked: false},
-		{name: "milk", checked: true},
-		{name: "bread", checked: false},
-		{name: "apples", checked: false}
+		{name: "oranges", checked: false, editing: false},
+		{name: "milk", checked: true, editing: false},
+		{name: "bread", checked: false, editing: false},
+		{name: "apples", checked: false, editing: false}
 	],
 	hideCompleted: false,
 	searchTerm: null,
@@ -21,19 +23,21 @@ function generateItemElement(item, itemIndex, template) {
 
 	if (STORE.searchTerm !== null) {
 		if (item.name.includes(STORE.searchTerm)) {
-			console.log(STORE.searchTerm);
 			return `
 			<li class="js-item-index-element ${hide ? "hidden" : ''}" data-item-index="${itemIndex}">
 				<span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
 				<div class="shopping-item-controls">
 					<button class="shopping-item-toggle js-item-toggle">
-							<span class="button-label">check</span>
+						<span class="button-label">check</span>
 					</button>
 					<button class="shopping-item-delete js-item-delete">
-							<span class="button-label">delete</span>
+						<span class="button-label">delete</span>
+					</button>
+					<button class="shopping-item-edit js-item-edit">
+						<span class="button-label">edit</span>
 					</button>
 				</div>
-			</li>`
+			</li>`;
 		}
 	} else {
 		return `
@@ -41,11 +45,14 @@ function generateItemElement(item, itemIndex, template) {
       <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
-            <span class="button-label">check</span>
+          <span class="button-label">check</span>
         </button>
         <button class="shopping-item-delete js-item-delete">
-            <span class="button-label">delete</span>
-        </button>
+          <span class="button-label">delete</span>
+				</button>
+				<button class="shopping-item-edit js-item-edit">
+					<span class="button-label">edit</span>
+				</button>
       </div>
     </li>`;
 	}
@@ -121,14 +128,10 @@ function handleCheckedFilter() {
 	});
 }
 
-// User can type in a search term and the displayed list will be filtered by item names only containing that search term
-
-// Search through items and return items that contain search input
 function searchItems(itemSearched) {
 	STORE.searchTerm = itemSearched;
 }
 
-// listen on submit and capture input--set input to ''
 function handleSearch() {
 	$('.js-search-form').submit(event => {
 		event.preventDefault();
@@ -138,17 +141,28 @@ function handleSearch() {
 		renderShoppingList();
 	});
 }
-// 
-
-
-
-
-
-
-
 
 // User can edit the title of an item
-// You must use the state management pattern. Update the store; run the render function. Do not directly update the DOM.
+// Create editing prop for each item set to false
+// Create edit btn when generating DOM
+// listen on ul for edit btn click
+// change editing prop to opp when clicked
+// in gen func if editing prop is true
+function handleEdit() {
+	$('.js-shopping-list').on('click', '.js-item-edit', event => {
+		console.log('clicked me bruh');
+	});
+}
+
+
+
+
+
+
+
+
+
+
 
 function handleShoppingList() {
   renderShoppingList();
@@ -157,6 +171,7 @@ function handleShoppingList() {
 	handleDeleteItemClicked();
 	handleCheckedFilter();
 	handleSearch();
+	handleEdit();
 }
 
 $(handleShoppingList);
